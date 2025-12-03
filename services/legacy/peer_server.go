95e03ed160ebce2394dca401a2e455e247660414
oskarszoon <1449115+oskarszoon@users.noreply.github.com>
2025-12-03 20:57:29 +0100

    Stream legacy blocks straight from asset

--- services/legacy/peer_server.go
+++ services/legacy/peer_server.go
@@ -13,7 +13,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"io"
 	"math"
 	"net"
 	"os"
@@ -31,7 +30,6 @@ import (
 	safeconversion "github.com/bsv-blockchain/go-safe-conversion"
 	txmap "github.com/bsv-blockchain/go-tx-map"
 	"github.com/bsv-blockchain/go-wire"
-	"github.com/bsv-blockchain/teranode/pkg/fileformat"
 	"github.com/bsv-blockchain/teranode/services/blockassembly"
 	"github.com/bsv-blockchain/teranode/services/blockchain"
 	"github.com/bsv-blockchain/teranode/services/blockvalidation"
@@ -1367,12 +1365,13 @@ func (s *server) getTxFromStore(hash *chainhash.Hash) (*bsvutil.Tx, int64, error
 // connected peer.  An error is returned if the block hash is not known.
 func (s *server) pushBlockMsg(sp *serverPeer, hash *chainhash.Hash, doneChan chan<- struct{},
 	waitChan <-chan struct{}, encoding wire.MessageEncoding) error {
-	// use a concurrent store to make sure we do not request the legacy block multiple times
-	// for different peers. This makes sure we serve the block from a local cache store and not from the utxo store.
-	reader, err := s.concurrentStore.Get(s.ctx, *hash, fileformat.FileTypeMsgBlock, func() (io.ReadCloser, error) {
-		url := fmt.Sprintf("%s/block_legacy/%s?wire=1", s.assetHTTPAddress, hash.String())
-		return util.DoHTTPRequestBodyReader(s.ctx, url)
-	})
+	// Stream directly from Asset Server without disk caching.
+	// For large blocks (4GB+), the disk I/O overhead of ConcurrentBlob causes timeouts.
+	// The Asset Server handles caching internally, so we stream directly to avoid:
+	// 1. Writing the entire block to disk (slow, causes context deadline exceeded)
+	// 2. Reading it back from disk (additional I/O overhead)
+	url := fmt.Sprintf("%s/block_legacy/%s?wire=1", s.assetHTTPAddress, hash.String())
+	reader, err := util.DoHTTPRequestBodyReader(s.ctx, url)
 	if err != nil {
 		sp.server.logger.Errorf("Unable to fetch requested block %v: %v", hash, err)
 
